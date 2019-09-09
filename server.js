968254ee353720d64aee59660a35a2301ec9ff47
Arvin Agas <arvagas@gmail.com>
2019-09-09 17:26:06 -0400

    added logger and server test

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require('express')
 
-const db = require('./data/dbConfig.js');
+const db = require('./data/dbConfig.js')
 
-const server = express();
+const server = express()
 
-server.use(express.json());
+server.use(express.json())
 
-module.exports = server;
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`)
+
+    next()
+}
+
+server.use(logger)
+
+server.get('/', (req, res) => {
+    res.json('Hello World')
+})
+
+module.exports = server
