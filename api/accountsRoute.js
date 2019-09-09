d7b18b9472e8659b1f347491b8edc74cddd34452
Arvin Agas <arvagas@gmail.com>
2019-09-09 17:41:27 -0400

    GET requests functional

--- api/accountsRoute.js
+++ api/accountsRoute.js
@@ -4,8 +4,24 @@ const router = express.Router()
 
 const db = require('../data/dbConfig.js')
 
-// @@@@@@@@@@ GET request @@@@@@@@@@
-// router.get()
+// @@@@@@@@@@ GET requests @@@@@@@@@@
+// Get all accounts
+router.get('/', (req, res) => {
+    db('accounts')
+    .then(accounts => res.json(accounts))
+    .catch(err => res.status(500).json(err))
+})
+
+//Get specific accounts
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('accounts')
+    .where({ id })
+    .first()
+    .then(account => res.json(account))
+    .catch(err => res.status(500).json(err))
+})
 
 // @@@@@@@@@@ POST request @@@@@@@@@@
 // router.post()
